267da687d46d0d6530ccd33fd334fea311abe758
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:55:58 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\Twilio;
 
-class TwilioModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	public function init() 
 	{
