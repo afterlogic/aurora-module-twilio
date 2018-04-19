390098fc8735cbc4505478e7e1e692d6d148d4ad
vasil <vasil.sokolov@gmail.com>
2018-04-19 14:34:49 +0300

    updated licensing annotations

--- Managers/Voice/Manager.php
+++ Managers/Voice/Manager.php
@@ -1,8 +1,5 @@
 <?php
 /**
- * @copyright Copyright (c) 2017, Afterlogic Corp.
- * @license AGPL-3.0 or AfterLogic Software License
- *
  * This code is licensed under AGPLv3 license or AfterLogic Software License
  * if commercial version of the product was purchased.
  * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
@@ -11,6 +8,10 @@
 namespace Aurora\Modules\Twilio\Managers\Voice;
 
 /**
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
+ * @license https://afterlogic.com/products/common-licensing AfterLogic Software License
+ * @copyright Copyright (c) 2018, Afterlogic Corp.
+ *
  * @package Voice
  */
 class Manager extends \Aurora\System\Managers\AbstractManager
